0e8b496b207a46ac88ec8fd748653eb6e655aa11
Raoul Luqué <raoulsluque@gmail.com>
2025-09-26 10:39:31 +0200

    Setup clap

--- src/bin/clox.rs
+++ src/bin/clox.rs
@@ -1,3 +1,24 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+/// A simple Lox interpreter and compiler written in Rust.
+#[derive(Parser)]
+struct Args {
+    /// The source file to interpret
+    source: Option<PathBuf>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    if let Some(source) = args.source {
+        println!("Interpreting source file: {:?}", source);
+        // Here you would add the logic to read and interpret the source file.
+        return;
+    } else {
+        println!("No source file provided. Entering REPL mode...");
+        // Here you would add the logic to start a REPL (Read-Eval-Print Loop).
+        return;
+    }
 }
---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+struct Lox;
+
+impl Lox {
+    fn new() -> Self {
+        Lox {}
+    }
+}
