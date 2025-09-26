f8f41987e105ad5c753e21f3b2d33668d72ff662
Raoul Luqué <raoulsluque@gmail.com>
2025-09-26 10:47:28 +0200

    Setup run_file and run_repl workflows

--- src/bin/clox.rs
+++ src/bin/clox.rs
@@ -1,6 +1,7 @@
 use std::path::PathBuf;
 
 use clap::Parser;
+use rustclox::Lox;
 
 /// A simple Lox interpreter and compiler written in Rust.
 #[derive(Parser)]
@@ -12,13 +13,13 @@ struct Args {
 fn main() {
     let args = Args::parse();
 
+    let mut lox = Lox::new();
+
     if let Some(source) = args.source {
-        println!("Interpreting source file: {:?}", source);
-        // Here you would add the logic to read and interpret the source file.
+        lox.run_file(&source);
         return;
     } else {
-        println!("No source file provided. Entering REPL mode...");
-        // Here you would add the logic to start a REPL (Read-Eval-Print Loop).
+        lox.run_repl();
         return;
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,30 @@
-struct Lox;
+use std::{
+    fs,
+    io::{self, Write},
+};
+
+pub struct Lox;
 
 impl Lox {
-    fn new() -> Self {
+    pub fn new() -> Self {
         Lox {}
     }
+
+    pub fn run_file(&mut self, path: &std::path::Path) -> std::io::Result<()> {
+        let source = fs::read_to_string(path)?;
+        self.run(&source);
+        Ok(())
+    }
+
+    pub fn run_repl(&mut self) -> std::io::Result<()> {
+        loop {
+            let mut input = String::new();
+            print!("> ");
+            io::stdout().flush()?;
+            std::io::stdin().read_line(&mut input)?;
+            self.run(&input);
+        }
+    }
+
+    pub fn run(&mut self, source: &str) {}
 }
